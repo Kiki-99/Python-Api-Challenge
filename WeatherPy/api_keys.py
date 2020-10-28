6d72e36263403f413d1e06eaffed86f2ebda15f9
Akilah Hunte <ahunt173@gmail.com>
2020-10-27 23:50:37 -0400

    Created Heat Map

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,8 +1,5 @@
 # OpenWeatherMap API Key
 weather_api_key = "59d3b9cd7111e5690c89506074de3cee"
 
-#Delete after tutoring
-temp_api_key = "3f1c7f058ec4a0b50ad58e07865910ba"
-
 # Google API Key
 g_key = "AIzaSyBs47axKJDmdPBrhWcwObk6XCohti7e5GU"
